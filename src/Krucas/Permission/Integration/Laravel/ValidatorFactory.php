b8673d8c85ddf88cc7b82afb7819f2513ab17602
Edvinas Kručas <edv.krucas@gmail.com>
2014-10-16 22:23:03 +0300

    Added laravel validator factory

---
+++ src/Krucas/Permission/Integration/Laravel/ValidatorFactory.php
@@ -0,0 +1,43 @@
+<?php namespace Krucas\Permission\Integration\Laravel;
+
+use Illuminate\Container\Container;
+use Krucas\Permission\ValidatorFactory as BaseValidatorFactory;
+
+class ValidatorFactory extends BaseValidatorFactory
+{
+    /**
+     * Used container instance.
+     *
+     * @var \Illuminate\Container\Container
+     */
+    protected $container;
+
+    /**
+     * @param \Illuminate\Container\Container $container
+     */
+    public function setContainer(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Return used container instance.
+     *
+     * @return \Illuminate\Container\Container
+     */
+    public function getContainer()
+    {
+        return $this->container;
+    }
+
+    /**
+     * Instantiate new object of given class.
+     *
+     * @param string $class Class to instantiate.
+     * @return \object
+     */
+    protected function instantiateClass($class)
+    {
+        return $this->getContainer()->make($class);
+    }
+}
